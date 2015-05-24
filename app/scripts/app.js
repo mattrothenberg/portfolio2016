6864fada4ef3efffd8be207a115b629ae4168c75
mattrothenberg <mattrothenberg@gmail.com>
2015-05-24 07:40:32 -0400

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mattportfolioApp
+ * @description
+ * # mattportfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mattportfolioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mattportfolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mattportfolioApp
+ */
+angular.module('mattportfolioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

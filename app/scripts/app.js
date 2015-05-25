fbc6d98d9e57602fb2fe6b9a38c605b00775c96e
mattrothenberg <mattrothenberg@gmail.com>
2015-05-25 19:36:57 -0400

    Resume Page initial dev

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,4 +16,20 @@ angular
     'ngSanitize',
     'ngTouch',
     'ui.router'
-  ]);
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise('/about');
+    //
+    // Now set up the states
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/main.html'
+      })
+      .state('resume', {
+        url: '/resume',
+        templateUrl: 'views/resume.html'
+      });
+  });

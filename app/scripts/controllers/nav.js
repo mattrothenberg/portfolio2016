78f9497253df293159607a5a66f72d825d89f840
mattrothenberg <mattrothenberg@gmail.com>
2016-03-18 08:08:53 -0400

    Replace `ui-sref-directive` with hand-rolled active class setter.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -8,5 +8,13 @@
  * Controller of the mattportfolioApp
  */
 angular.module('mattportfolioApp')
-  .controller('NavCtrl', function () {
+  .controller('NavCtrl', function ($scope, $rootScope) {
+
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams, options) {
+      if ( toState.name === 'work.design' || toState.name === 'work.development') {
+        $scope.isSubPage = true;
+      } else {
+        $scope.isSubPage = false;
+      }
+    })
   });
